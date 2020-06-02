0dfa9c0d41b9150eb94d34de646712d504b2ee03
NicolineRusbak <1081543@ucn.dk>
2020-06-02 15:24:24 +0200

    Vis mere boks på medlemsbetingelser

--- assets/js/crossfit-js.js
+++ assets/js/crossfit-js.js
@@ -1,1 +1,18 @@
-document.getElementById("demo").innerHTML = "Hello JavaScript!";
+//document.getElementById("demo").innerHTML = "Hello JavaScript!";
+
+
+// Bokse til at vise mere tekst når men klikker 
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
