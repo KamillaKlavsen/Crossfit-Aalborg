a1aafb9055e1fa9707721f2fb0a5c9c44e0ea37c
KamillaKlavsen <kamillaklavsen@outlook.dk>
2020-06-03 14:49:05 +0200

    Forsøg på modal + kopi holdbeskriv

--- assets/js/crossfit-js.js
+++ assets/js/crossfit-js.js
@@ -1,5 +1,3 @@
-//document.getElementById("demo").innerHTML = "Hello JavaScript!";
-
 
 // Bokse til at vise mere tekst når men klikker 
 let acc = document.getElementsByClassName("accordion");
@@ -15,4 +13,32 @@ for (i = 0; i < acc.length; i++) {
       panel.style.display = "block";
     }
   });
+}
+
+// Modal til holdbeskrivelser
+
+// Get the modal
+var bandModalId = document.getElementById("bandModalId");
+
+// Get the button that opens the modal
+var btn = document.getElementById("bands");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  bandModalId.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  bandModalId.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == bandModalId) {
+    bandModalId.style.display = "none";
+  }
 }
