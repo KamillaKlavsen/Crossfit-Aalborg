6db8683068e948cdc39a6b028a013d79e9ec84ba
KamillaKlavsen <kamillaklavsen@outlook.dk>
2020-06-04 15:00:10 +0200

    et modal virker - dog uden luk

--- assets/js/crossfit-js.js
+++ assets/js/crossfit-js.js
@@ -17,7 +17,7 @@ for (i = 0; i < acc.length; i++) {
 
 
 
-/*
+
 // Modal til holdbeskrivelser
 
 //OPEN GYM
@@ -48,29 +48,29 @@ window.onclick = function(event) {
 }
 
 //MASTER
-var holdModalId1 = document.getElementById("holdModalId1");
+// var holdModalId1 = document.getElementById("holdModalId1");
 
-// Get the button that opens the modal
-var btn1 = document.getElementById("hold1");
+// // Get the button that opens the modal
+// var btn1 = document.getElementById("hold1");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[1];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[1];
 
-// When the user clicks the button, open the modal 
-btn1.onclick = function() {
-  holdModalId1.style.display = "block";
-}
+// // When the user clicks the button, open the modal 
+// btn1.onclick = function() {
+//   holdModalId1.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  holdModalId1.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   holdModalId1.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == holdModalId1) {
-    holdModalId1.style.display = "none";
-  }
-}*/
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == holdModalId1) {
+//     holdModalId1.style.display = "none";
+//   }
+// }
 
 
