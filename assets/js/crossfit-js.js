f48fd4a17e164dead7fb79a627e460123ef2a187
KamillaKlavsen <kamillaklavsen@outlook.dk>
2020-06-05 13:07:55 +0200

    holdbeskriv kopi 2 - modaler driller stadig

--- assets/js/crossfit-js.js
+++ assets/js/crossfit-js.js
@@ -20,58 +20,32 @@ for (i = 0; i < acc.length; i++) {
 
 // Modal til holdbeskrivelser
 
+//ved hjælp fra følgende video https://www.youtube.com/watch?v=Ifx85VUGQIY
+
 //OPEN GYM
-// Get the modal
-let holdModalId = document.getElementById("holdModalId");
+let modalBtns = document.querySelectorAll('.modal-open');
 
-// Get the button that opens the modal
-let btn = document.getElementById("hold");
+modalBtns.forEach(function(btn) {
+  btn.onclick = function(){
+    let modal = btn.getAttribute('data-modal');
 
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+    document.getElementById(modal).style.display = 'block';
+  };
+});
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  holdModalId.style.display = "block";
-}
+let closeBtns = document.querySelectorAll('.modal-close');
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  holdModalId.style.display = "none";
-}
+closeBtns.forEach(function(btn){
+  btn.onclick = function(){
+    let modal = (btn.closest('.modal').style.display = 'none');
+  };
+});
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == holdModalId) {
-    holdModalId.style.display = "none";
+window.onclick = function(event){
+  if(event.target.className === 'modal'){
+    event.target.style.display = 'none';
   }
 }
-
-//MASTER
-// var holdModalId1 = document.getElementById("holdModalId1");
-
-// // Get the button that opens the modal
-// var btn1 = document.getElementById("hold1");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[1];
-
-// // When the user clicks the button, open the modal 
-// btn1.onclick = function() {
-//   holdModalId1.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   holdModalId1.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == holdModalId1) {
-//     holdModalId1.style.display = "none";
-//   }
-// }
 
 
 // TEST
